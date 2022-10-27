db51b02350582f0407b8f5c439131fdc57049f80
Diogo Martins <up202108883@up.pt>
2022-10-27 11:10:00 +0100

    Created groovy tests for ListAggregator class

--- src/test/groovy/com/aor/numbers/ListAggregatorSpock.groovy
+++ src/test/groovy/com/aor/numbers/ListAggregatorSpock.groovy
@@ -4,26 +4,46 @@ import spock.lang.Specification
 
 class ListAggregatorSpock extends Specification {
     private def testList
+    private def testAggregator
 
     def setup() {
         testList = Arrays.asList(1, 2, 4, 2, 6, -7, 5, 4, 7, -4)
+        testAggregator = new ListAggregator()
+    }
+
+    def "sum"() {
+        when:
+            def result = testAggregator.sum(testList)
+
+        then:
+            result == 20
+    }
+
+    def "max"() {
+        when:
+            def result = testAggregator.max(testList)
+
+        then:
+            result == 7
+    }
+
+    def "min"() {
+        when:
+            def result = testAggregator.min(testList)
+
+        then:
+            result == -7
     }
 
     def "distinct"() {
         given:
-            def aggregator
-    }
-
-    def "disting_bug_8726"() {
-        given:
-            def aggregator = Mock(ListAggregator)
             def deduplicator = Mock(GenericListDeduplicator)
-            deduplicator.deduplicate(Arrays.asList(1, 2, 4, 2)) >> Arrays.asList(1, 2, 4)
+            deduplicator.deduplicate(testList) >> Arrays.asList(-7, -4, 1, 2, 4, 5, 6, 7)
 
         when:
-            def result = aggregator.distinct(Arrays.asList(1, 2, 4, 2))
+            def result = testAggregator.distinct(testList, deduplicator)
 
         then:
-            result == Arrays.asList(1, 2, 4)
+            result == 8
     }
 }
